e583457065e9843617b39c6d1dba4527a9569f38
senecala <aaron.senecal@oregonstate.edu>
2011-10-18 16:22:20 -0700

    Quick update to page template: default header-top links display when no block is present

--- page.tpl.php
+++ page.tpl.php
@@ -74,15 +74,17 @@ if ($logo == $base_path.$directory.'/logo.png')
               </div><!-- /osu-top-hat -->
 
               <div id="header-top-links">
-                <?php if (variable_get('extension_settings_site_type', 0) != 4) : ?>
-                 <!-- 
+                <?php if (variable_get('extension_settings_site_type', 0) != 4) {
+					if (stristr(theme('grid_row', $header_top, 'header-top-region', 'full-width', $grid_width), 'header-top-link') == FALSE) { ?>
+
 				 <a href="http://extension.oregonstate.edu/node/312">Donate</a>
                   &nbsp; |&nbsp;&nbsp;
                   <a href="http://extension.oregonstate.edu/node/3248">Calendar</a>
                   &nbsp;&nbsp;|&nbsp;&nbsp;
                   <a href="http://outreach.oregonstate.edu">Outreach &amp; Engagement</a>
-				-->
-                <?php endif; ?>
+
+                <?php }
+				} ?>
               </div>
               <?php print theme('grid_row', $header_top, 'header-top-region', 'full-width', $grid_width); ?>
               <?php print theme('grid_block', $search_box, 'search-box'); ?>
