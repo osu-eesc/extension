a8ac7e45b3886a079a97ca08244d1249814ad99b
Bryan Mayjor <bmayjor@gmail.com>
2012-01-10 14:42:45 -0800

    corrected search form css, added js for search box

--- page.tpl.php
+++ page.tpl.php
@@ -5,6 +5,7 @@ if ($logo == $base_path . $directory.'/logo.png')
 	$logo = $base_path . $directory.'/logo.jpg';
 }
 $body_classes .= ' site-type-'.variable_get('extension_settings_site_type', 0);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>" id="html-alpha">
--- template.php
+++ template.php
@@ -2,40 +2,6 @@
 // $Id: template.php,v 1.5 2010/09/17 21:36:06 eternalistic Exp $
 
 /**
- * Changed breadcrumb separator
- */
-function acquia_prosper_breadcrumb($breadcrumb) {
-  if (!empty($breadcrumb)) {
-    return '<div class="breadcrumb">'. implode(' &rarr; ', $breadcrumb) .'</div>';
-  }
-}
-
-
-/**
- * Theme override for theme_menu_item()
- * Adds unique classes and ids for menu items
- * Can be used for image replacement
- */
-function eesc_prosper_menu_item($link, $has_children, $menu = '', $in_active_trail = FALSE, $extra_class = NULL) {
-  $class = ($menu ? 'expanded' : ($has_children ? 'collapsed' : 'leaf'));
-  if (!empty($extra_class)) {
-    $class .= ' '. $extra_class;
-  }
-  if ($in_active_trail) {
-    $class .= ' active-trail';
-  }
- 
-  // Add unique identifier
-  static $item_id = 0;
-  $item_id += 1;
-  $id .= ''. 'menu-item-custom-id-' . $item_id;
-  // Add semi-unique class
-  $class .= ' ' . preg_replace("/[^a-zA-Z0-9]/", "", strip_tags($link));
- 
-  return '<li class="'. $class .'" id="' . $id . '">'. $link . $menu ."</li>\n";
-}
-
-/**
  * Theme override for theme_preprocess_page()
  * Adds template suggestion per content type
  * Also set a variable for a taxonomy term and tid 
